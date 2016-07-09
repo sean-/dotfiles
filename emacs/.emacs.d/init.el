e48a60c11df0a764bcf41e297f5b6555779af412
Sean Chittenden <sean@chittenden.org>
2016-07-08 23:16:00 -0700

    Add git-gutter.el+

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -363,3 +363,25 @@ nil (server-start))
 ;; prominent keybdining.
 (global-set-key (kbd "C-\\") 'yafolding-toggle-element)
 (global-set-key (kbd "M-\\") 'yafolding-toggle-all)
+
+;; Enable git-gutter+ when in prog-mode
+(add-hook 'prog-mode-hook 'git-gutter+-mode)
+(global-set-key (kbd "C-x g") 'git-gutter+-mode) ; Turn on/off in the current buffer
+(global-set-key (kbd "C-x G") 'global-git-gutter+-mode) ; Turn on/off globally
+
+(eval-after-load 'git-gutter+
+  '(progn
+     ;;; Jump between hunks
+     (define-key git-gutter+-mode-map (kbd "C-x n") 'git-gutter+-next-hunk)
+     (define-key git-gutter+-mode-map (kbd "C-x p") 'git-gutter+-previous-hunk)
+
+     ;;; Act on hunks
+     (define-key git-gutter+-mode-map (kbd "C-x v =") 'git-gutter+-show-hunk)
+     (define-key git-gutter+-mode-map (kbd "C-x r") 'git-gutter+-revert-hunks)
+     ;; Stage hunk at point.
+     ;; If region is active, stage all hunk lines within the region.
+     (define-key git-gutter+-mode-map (kbd "C-x t") 'git-gutter+-stage-hunks)
+     (define-key git-gutter+-mode-map (kbd "C-x c") 'git-gutter+-commit)
+     (define-key git-gutter+-mode-map (kbd "C-x C") 'git-gutter+-stage-and-commit)
+     (define-key git-gutter+-mode-map (kbd "C-x C-y") 'git-gutter+-stage-and-commit-whole-buffer)
+     (define-key git-gutter+-mode-map (kbd "C-x U") 'git-gutter+-unstage-whole-buffer)))
