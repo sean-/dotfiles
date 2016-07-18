0a4413f8d5993f827147d4f2652c97037819ba90
Sean Chittenden <sean@chittenden.org>
2016-07-18 09:04:42 -0700

    Swap the order of send/receive for mutt

--- mutt/.mutt/bin/send-receive-all.sh
+++ mutt/.mutt/bin/send-receive-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh --
 
-mbsync -a &
-exec /usr/bin/env PATH=$HOME/.mail/bin:$PATH $HOME/.mail/bin/msmtp-queue  -r
+/usr/bin/env PATH=$HOME/.mail/bin:$PATH $HOME/.mail/bin/msmtp-queue  -r &
+exec mbsync -a
